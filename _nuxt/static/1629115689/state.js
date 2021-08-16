f5e31ec6df62aabd0c9b602463e698ec9ea3428a
Andrew Maksimchuk <amaksimchuk@bigmir.net>
2021-08-16 15:09:12 +0300

    7c43d45

---
+++ _nuxt/static/1629115689/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmixlab\u002F_nuxt\u002Fstatic\u002F1629115689",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fmixlab\u002F",assetsPath:"\u002Fmixlab\u002F_nuxt\u002F",cdnURL:a}}}}(null));
